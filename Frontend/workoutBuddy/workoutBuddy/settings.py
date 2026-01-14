56c10daaebbd065293fd8e08bcde64dc2330e1e6
yatharthgill <yatharthchaudhary05@gmail.com>
2026-01-14 22:17:57 +0530

    Update CSRF trusted origins and add sample .env file for backend configuration

--- Frontend/workoutBuddy/workoutBuddy/settings.py
+++ Frontend/workoutBuddy/workoutBuddy/settings.py
@@ -26,7 +26,10 @@ ALLOWED_HOSTS = [
 
 CSRF_TRUSTED_ORIGINS = [
     "https://workoutbuddyfrontend-49zr.onrender.com",
-    "https://workoutbuddy-zbz4.onrender.com"
+    "https://workoutbuddy-zbz4.onrender.com",
+     "https://workoutbuddyfrontend-kn9e.onrender.com",
+     "https://workoutbuddy-9nqn.onrender.com"
+
 ]
 
 # Application definition
